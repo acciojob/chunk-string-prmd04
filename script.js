55181d6540d0e6f2ae1f391f93b48d73021fa5a8
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-05-19 20:45:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-function chunkString(str, chunkLength) {
+function stringChop(str, size) {
     const chunks = [];
-
-    for (let i = 0; i < str.length; i += chunkLength) {
-        chunks.push(str.slice(i, i + chunkLength));
+    for (let i = 0; i < str.length; i += size) {
+        chunks.push(str.slice(i, i + size));
     }
-
     return chunks;
 }
 
-// Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+const str = prompt("Enter String:");
+const size = parseInt(prompt("Enter Chunk Size:"), 10);
+
+if (isNaN(size) || size <= 0) {
+    alert("Invalid chunk size. Please enter a positive number.");
+} else {
+    alert("Chunks: " + stringChop(str, size).join(", "));
+}
